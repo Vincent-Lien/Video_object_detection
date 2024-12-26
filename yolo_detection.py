467802a47450310bd2333cc9ec6f186ab4831cbf
Vincent-Lien <vincentlien0606@gmail.com>
2024-12-26 06:47:07 +0000

    add: yolo detection method

--- yolo_detection.py
+++ yolo_detection.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 # Load a pretrained YOLO11n model
-model = YOLO("yolo11x-seg.pt")
+model_weight_path = "yolo11x.pt"
+model = YOLO(model_weight_path)
 
 # Define path to video file
 source = "person_cats_V3.mp4"
@@ -16,7 +17,7 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 
 # Get video writer initialized to save the output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output_seg_conf_0.4.mp4', fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
+out = cv2.VideoWriter('output_yolo.mp4', fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
 
 # Run inference on the source
 
@@ -29,30 +30,11 @@ while cap.isOpened():
     person_count = 0
     cat_count = 0
     
-    for result in results:
-        # Draw segmentation masks
-        if result.masks is not None:
-            for seg, cls, conf in zip(result.masks.xy, result.boxes.cls, result.boxes.conf):
-                if conf < 0.4:
-                    continue
-                if cls == 0:
-                    color = (0, 0, 255)  # Red
-                    person_count += 1
-                elif cls == 15:
-                    color = (255, 0, 255)  # Purple
-                    cat_count += 1
-                else:
-                    continue
-
-                overlay = frame.copy()
-                points = np.array(seg, dtype=np.int32)
-                cv2.fillPoly(overlay, [points], color)
-                frame = cv2.addWeighted(overlay, 0.4, frame, 0.6, 0)
-
+    for result in results:        
         # Draw bounding boxes
         for box in result.boxes:
-            confidence = float(box.conf[0])
-            if confidence < 0.4:
+            conf = float(box.conf[0])
+            if conf < 0.1:
                 continue
             
             if box.cls in [0, 15]:
@@ -60,17 +42,19 @@ while cap.isOpened():
                 label = result.names[int(box.cls)]
                 
                 if box.cls == 0:  # Person
-                    color = (0, 0, 255)
+                    color = (0, 0, 255) # Red
+                    person_count += 1
                 elif box.cls == 15:  # Cat
-                    color = (255, 0, 255)
-                
+                    color = (255, 0, 255)   # Magenta
+                    cat_count += 1
+            
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                cv2.putText(frame, f'{label} {confidence:.2f}', (x1, y1 - 10),
+                cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
     
     # Add text to the top-left corner of the frame in red and bold, split into three lines
     cv2.putText(frame, '313581009', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, f'Persons: {person_count}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, f'Person: {person_count}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, f'Cats: {cat_count}', (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     out.write(frame)
 
